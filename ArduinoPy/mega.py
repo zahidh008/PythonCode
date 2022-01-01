d5cbbefad0599afa9ca56b77a4e095a9aa7c705f
zahidh008 <ito.zahidh@gmail.com>
2022-01-01 12:54:20 +0600

    arduino

---
+++ ArduinoPy/mega.py
@@ -0,0 +1,21 @@
+# Import library for arduino code
+
+
+#Read data form wifi
+
+# weather report// control value// 
+
+
+#Write data to wifi
+
+#Control value// Sensor value
+
+
+#Read data form GSM
+
+#call and sms
+
+
+#Write data to GSM
+
+#call and sms
