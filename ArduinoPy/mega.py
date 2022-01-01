0508e615d72f7c96f95eb89c8e0bde0bb53feada
zahidh008 <96931659+zahidh008@users.noreply.github.com>
2022-01-01 16:09:01 +0600

    Update mega.py

--- ArduinoPy/mega.py
+++ ArduinoPy/mega.py
@@ -2,20 +2,40 @@
 
 
 #Read data form wifi
-
 # weather report// control value// 
 
 
-#Write data to wifi
 
+#Write data to wifi
 #Control value// Sensor value
 
 
-#Read data form GSM
 
+#Read data form GSM
 #call and sms
+
 
 
 #Write data to GSM
-
 #call and sms
+
+
+
+#Read data from sensors
+
+
+
+
+
+
+
+#Routine
+#Morning
+#Alarm based on the day
+print("Alarm ringing")
+
+
+
+
+
+
