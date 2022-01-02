06b1d3550a52708a3577070335235c8c5b7e1fcd
zahidh008 <ito.zahidh@gmail.com>
2022-01-02 13:23:29 +0600

    a

--- ArduinoPy/mega.py
+++ ArduinoPy/mega.py
@@ -16,8 +16,42 @@ import json
 
 #Read data form GSM
 #call and sms
-receivedsms = ""
+textcomands = ["show command", "status", "led on", "led off", "fan on", "fan off"]
+text = ["show command", "status", "led on", "led off"]
+receivedsms = "show all command"
+receivedsms = receivedsms.lower()
 
+for a in textcomands:
+    b = a.split(' ')
+    for c in b:
+        if receivedsms.find(c) != -1:
+            match = 1
+        else:
+            match = 0
+            break
+    if match == 1:
+        print("Matched. Execute- ")
+        # print(a)
+        if a == "show command":
+            print("Commands are: ")
+            for a in textcomands:
+                print(a)
+        elif a == "status":
+            print("status")
+        elif a == "led on":
+            print("led on")
+        elif a == "led off":
+            print("led off")
+        elif a == "fan on":
+            print("fan on")
+        elif a == "fan off":
+            print("fan off")
+        
+        
+
+
+# if receivedsms.find('STATUS') != -1:
+#     print("show status") #show status
 
 
 #Write data to GSM
